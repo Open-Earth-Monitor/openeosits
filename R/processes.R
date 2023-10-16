4ad26c5dade8fe0fc502eec90f4e5c7b0b7d4ce9
Brian Pondi <brian.pondi@uni-muenster.de>
2023-10-16 14:24:07 +0200

    sits replacing gdalcubes on load collection

--- R/processes.R
+++ R/processes.R
@@ -5,6 +5,7 @@
 #' @import rstac
 #' @import useful
 #' @import sf
+#' @import parallel
 NULL
 
 #' schema_format
@@ -15,26 +16,29 @@ NULL
 #'
 #' @return list with type and subtype(optional)
 #' @export
-schema_format = function(type, subtype = NULL, items = NULL) {
-  schema = list()
-  schema = append(schema,list(type=type))
+schema_format <- function(type, subtype = NULL, items = NULL) {
+  schema <- list()
+  schema <- append(schema, list(type = type))
 
   if (!is.null(subtype) && !is.na(subtype)) {
-    schema = append(schema, list(subtype = subtype))
+    schema <- append(schema, list(subtype = subtype))
   }
   if (!is.null(items) && !is.na(items)) {
-    schema = append(schema, list(items = items))
+    schema <- append(schema, list(items = items))
   }
   return(schema)
 }
+
+#' detect the number of CPU cores on the current host
+CORES <- parallel::detectCores()
 
 
 #' datacube_schema
 #' @description Return a list with datacube description and schema
 #'
 #' @return datacube list
-datacube_schema = function() {
-  info = list(
+datacube_schema <- function() {
+  info <- list(
     description = "A data cube for further processing",
     schema = list(type = "object", subtype = "raster-cube")
   )
@@ -42,11 +46,11 @@ datacube_schema = function() {
 }
 
 #' return object for the processes
-eo_datacube = datacube_schema()
+eo_datacube <- datacube_schema()
 
 
 #' load collection
-load_collection = Process$new(
+load_collection <- Process$new(
   id = "load_collection",
   description = "Loads a collection from the current back-end by its id and returns it as processable data cube",
   categories = as.array("cubes", "import"),
@@ -57,7 +61,8 @@ load_collection = Process$new(
       description = "The collection id",
       schema = list(
         type = "string",
-        subtype = "collection-id")
+        subtype = "collection-id"
+      )
     ),
     Parameter$new(
       name = "spatial_extent",
@@ -70,114 +75,85 @@ load_collection = Process$new(
           properties = list(
             east = list(
               description = "East (upper right corner, coordinate axis 1).",
-              type = "number"),
+              type = "number"
+            ),
             west = list(
               description = "West lower left corner, coordinate axis 1).",
-              type = "number"),
+              type = "number"
+            ),
             north = list(
               description = "North (upper right corner, coordinate axis 2).",
-              type = "number"),
+              type = "number"
+            ),
             south = list(
               description = "South (lower left corner, coordinate axis 2).",
-              type = "number")
+              type = "number"
+            )
           ),
-        required = c("east", "west", "south", "north")
-      ),
-      list(
-        title = "GeoJson",
-        type = "object",
-        subtype = "geojson"
-      ),
-      list(
-        title = "No filter",
-        description = "Don't filter spatially. All data is included in the data cube.",
-        type = "null"
+          required = c("east", "west", "south", "north")
+        ),
+        list(
+          title = "GeoJson",
+          type = "object",
+          subtype = "geojson"
+        ),
+        list(
+          title = "No filter",
+          description = "Don't filter spatially. All data is included in the data cube.",
+          type = "null"
+        )
       )
-     )
     ),
     Parameter$new(
       name = "temporal_extent",
       description = "Limits the data to load from the collection to the specified left-closed temporal interval.",
       schema = list(
         type = "array",
-        subtype = "temporal-interval")
+        subtype = "temporal-interval"
+      )
     ),
     Parameter$new(
       name = "bands",
       description = "Only adds the specified bands into the data cube so that bands that don't match the list of band names are not available.",
       schema = list(
-        type = "array"),
+        type = "array"
+      ),
       optional = TRUE
     )
   ),
   returns = eo_datacube,
   operation = function(id, spatial_extent, temporal_extent, bands = NULL, job) {
-     # Temporal extent preprocess
-    t0 = temporal_extent[[1]]
-    t1 = temporal_extent[[2]]
-    duration = c(t0, t1)
-    time_range = paste(duration, collapse="/")
-    message("....After Temporal extent")
-
-    # spatial extent for cube view
-    xmin = as.numeric(spatial_extent$west)
-    ymin = as.numeric(spatial_extent$south)
-    xmax = as.numeric(spatial_extent$east)
-    ymax = as.numeric(spatial_extent$north)
-    message("...After Spatial extent")
-
-    # spatial extent for stac call
-    xmin_stac = xmin
-    ymin_stac = ymin
-    xmax_stac = xmax
-    ymax_stac = ymax
-    message("....After default Spatial extent for stac")
-    if(crs != 4326){
-      message("....crs is not 4326")
-      min_pt <- sf::st_sfc(st_point(c(xmin, ymin)), crs = crs)
-      min_pt <- sf::st_transform(min_pt, crs = 4326)
-      min_bbx <- sf::st_bbox(min_pt)
-      xmin_stac <- min_bbx$xmin
-      ymin_stac <- min_bbx$ymin
-      max_pt <- sf::st_sfc(st_point(c(xmax, ymax)), crs = crs)
-      max_pt <- sf::st_transform(max_pt, crs = 4326)
-      max_bbx <- sf::st_bbox(max_pt)
-      xmax_stac <- max_bbx$xmax
-      ymax_stac <- max_bbx$ymax
-      message("....transformed to 4326")
+    source_id <- NULL
+    if (id == "SENTINEL-2-L2A") {
+      source_id <- "AWS"
+    } else {
+      stop("The image collection specified is  not supported.")
     }
 
-    # Connect to STAC API and get satellite data
-    message("STAC API call.....")
-    stac_object <- stac("https://earth-search.aws.element84.com/v0")
-    items <- stac_object %>%
-      stac_search(
-        collections = id,
-        bbox = c(xmin_stac, ymin_stac, xmax_stac, ymax_stac),
-        datetime =  time_range,
-        limit = 10000
-      ) %>%
-      post_request() %>%
-      items_fetch()
-    # create image collection from stac items features
-    img.col <- stac_image_collection(items$features, property_filter =
-                                       function(x) {x[["eo:cloud_cover"]] < 30})
-    # Define cube view with monthly aggregation
-     crs <- c("EPSG", crs)
-     crs <- paste(crs, collapse=":")
-     v.overview <- cube_view(srs=crs, dx=pixels_size, dy=pixels_size, dt=time_aggregation,
-                  aggregation="median", resampling = "average",
-                  extent=list(t0 = t0, t1 = t1,
-                              left=xmin, right=xmax,
-                              top=ymax, bottom=ymin))
-    # gdalcubes creation
-    cube <- raster_cube(img.col, v.overview)
+    # Temporal extent
+    t0 <- temporal_extent[[1]]
+    t1 <- temporal_extent[[2]]
 
-    if(! is.null(bands)) {
-      cube = select_bands(cube, bands)
-    }
-    message("Data Cube is created....")
-    message(as_json(cube))
+    # spatial extent  of roi
+    xmin <- as.numeric(spatial_extent$west)
+    ymin <- as.numeric(spatial_extent$south)
+    xmax <- as.numeric(spatial_extent$east)
+    ymax <- as.numeric(spatial_extent$north)
+
+    # roi
+    roi_bounds <- c(
+      lon_min = xmin, lat_min = ymin,
+      lon_max = xmax, lat_max = ymax
+    )
+    # get irregular data cube from ARD image collection
+    cube <- sits::sits_cube(
+      source = source_id,
+      collection = id,
+      roi = roi_bounds,
+      bands = bands,
+      start_date = t0,
+      end_date = t1
+    )
     return(cube)
   }
 )
@@ -185,7 +161,7 @@ load_collection = Process$new(
 
 
 #' save result
-save_result = Process$new(
+save_result <- Process$new(
   id = "save_result",
   description = "Saves processed data to the local user workspace / data store of the authenticated user.",
   categories = as.array("cubes", "export"),
@@ -196,21 +172,24 @@ save_result = Process$new(
       description = "The data to save.",
       schema = list(
         type = "object",
-        subtype = "raster-cube")
+        subtype = "raster-cube"
+      )
     ),
     Parameter$new(
       name = "format",
       description = "The file format to save to.",
       schema = list(
         type = "string",
-        subtype = "output-format")
+        subtype = "output-format"
+      )
     ),
     Parameter$new(
       name = "options",
       description = "The file format parameters to be used to create the file(s).",
       schema = list(
         type = "object",
-        subtype = "output-format-options"),
+        subtype = "output-format-options"
+      ),
       optional = TRUE
     )
   ),
@@ -219,10 +198,8 @@ save_result = Process$new(
     schema = list(type = "boolean")
   ),
   operation = function(data, format, options = NULL, job) {
-    gdalcubes_options(parallel = 8)
     message("Data is being saved in format :")
     message(format)
-    message("The above format is being saved")
     job$setOutput(format)
     return(data)
   }
